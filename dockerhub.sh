5216a1a464e52126dfbe5f81798d4e4d1cfdbaee
James Lockhart <james.lockhart@sykescottages.co.uk>
2023-05-04 10:57:12 +0100

    Improved the build and test pipeline scripts

--- dockerhub.sh
+++ dockerhub.sh
@@ -5,10 +5,7 @@ LATEST_VERSION="3.11"
 # You need to provide your own creds because #security
 docker login
 
-docker build --no-cache -t sykescottages/python:base base
-docker push sykescottages/python:base
-
-VERSIONS=( "2.7" "3.6" "3.7" "3.8" "3.9" "3.10" "3.11" )
+VERSIONS=( "base" "base-legacy" "2.7" "3.6" "3.7" "3.8" "3.9" "3.10" "3.11" )
 for VERSION in "${VERSIONS[@]}"
 do
   docker build --no-cache -t sykescottages/python:${VERSION} $VERSION
--- test.sh
+++ test.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
-docker-compose -f base/docker-compose.test.yml build --no-cache &&
-  docker-compose -f base/docker-compose.test.yml run --rm sut
-
-if [ $? -eq 0 ]; then
-  echo "Base Image Test Passed"
-else
-  echo "Base Image Test Failed"
-  exit
-fi
-
-VERSIONS=("2.7" "3.6" "3.7" "3.8" "3.9" "3.10" "3.11")
+VERSIONS=("base" "base-legacy" "2.7" "3.6" "3.7" "3.8" "3.9" "3.10" "3.11")
 for VERSION in "${VERSIONS[@]}"; do
   docker-compose -f $VERSION/docker-compose.test.yml build --no-cache &&
     docker-compose -f $VERSION/docker-compose.test.yml run --rm sut
