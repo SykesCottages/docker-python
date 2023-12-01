a1cc232e92b0dd3319268790807bfe3e92f0d6f1
James Lockhart <james.lockhart@sykescottages.co.uk>
2023-12-01 17:48:25 +0000

    Added all the builds back

--- dockerhub.sh
+++ dockerhub.sh
@@ -10,7 +10,7 @@ docker login >> /dev/null 2>&1
 
 wait
 
-VERSIONS=("3.10" "3.11" "3.12") #$(find . -type d -regex '.*/[0-9]+\.[0-9]' | sort | sed 's/\.\///'))
+VERSIONS=(find . -type d -regex '.*/[0-9]+\.[0-9]' | sort | sed 's/\.\///'))
 for VERSION in "${VERSIONS[@]}"
 do
   ./build.sh $VERSION $LATEST_VERSION >> /dev/null 2>&1 &
